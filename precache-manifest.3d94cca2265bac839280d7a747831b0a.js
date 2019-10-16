f6599542de5dc0d714ea747ffbe2743436f27e42
tarverdarius@gmail.com <Tarverdarius@gmail.com>
2019-10-16 19:28:08 -0400

    Updates

---
+++ precache-manifest.3d94cca2265bac839280d7a747831b0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d59ecf83be77b92e45787b6213c9bfc",
+    "url": "/clicky_react/index.html"
+  },
+  {
+    "revision": "da73ced6068a5efe1778",
+    "url": "/clicky_react/static/css/main.8d2ffb65.chunk.css"
+  },
+  {
+    "revision": "444265842b869bf1f4f7",
+    "url": "/clicky_react/static/js/2.03eef2fc.chunk.js"
+  },
+  {
+    "revision": "da73ced6068a5efe1778",
+    "url": "/clicky_react/static/js/main.8a229e02.chunk.js"
+  },
+  {
+    "revision": "b0f54c38e8d48ff65ede",
+    "url": "/clicky_react/static/js/runtime-main.6e06db6b.js"
+  }
+]);
