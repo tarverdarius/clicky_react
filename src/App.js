07bba58251b40812fae34253cee71a87eb15919d
tarverdarius@gmail.com <Tarverdarius@gmail.com>
2019-10-26 20:49:33 -0400

    final changes

--- src/App.js
+++ src/App.js
@@ -27,22 +27,20 @@ class App extends Component {
     return true;
   }
 
-  clickCount = id => {
-    this.state.cards.find((o, i) => {
-      if (o.id === id) {
-        if(cards[i].count === 0){
+  handleClickCount = id => {
+    this.state.cards.find((image, i) => {
+      if (image.id === id) {
+        if (cards[i].count === 0) {
           cards[i].count = cards[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
-            console.log(this.state.score);
-          });
-          this.state.cards.sort(() => Math.random() - 0.5)
-          return true; 
+          this.setState({ score: this.state.score + 1 });
+          this.state.cards.sort(() => Math.random() - 0.5);
+          return true;
         } else {
           this.gameOver();
         }
       }
     });
-  }
+  };
   // Map over this.state.cards and render a cardCard component for each card object
   render() {
     return (
