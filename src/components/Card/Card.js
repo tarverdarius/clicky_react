e133a1db0bb5a04a4dd5744cf551af4aa4b980ce
tarverdarius@gmail.com <Tarverdarius@gmail.com>
2019-10-26 21:03:06 -0400

    changing function in card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.css";
 
 const Card = props => (
-  <div className="card" onClick={() => props.clickCount(props.id)}>
+  <div className="card" onClick={() => props.handleClickCount(props.id)}>
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
